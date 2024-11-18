e9730da38443047ba4c10704f1e81ceb92857ba5
codeplaygroundspace <rosinaaa@gmail.com>
2024-11-18 11:17:35 +0100

    catch error

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const renderCountry = function (data, className = "") {
   const name = data.name.common;
   const flag = data.flags.svg;
   const region = data.region;
-  const language = Object.values(data.languages)[0];
-  const currency = Object.values(data.currencies)[0].name;
+  const language = Object.values(data.languages || {})[0] || "None";
+  const currency = Object.values(data.currencies || {})[0]?.name || "None";
 
   const html = `
   <article class="country ${className}">
@@ -33,35 +33,24 @@ const renderError = function (msg) {
   countriesContainer.style.opacity = 1;
 };
 
-const getPosition = function () {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
-  });
+const getCountryData = function (country) {
+  //Country 1
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+
+      //Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0], "neighbour"))
+    .catch((err) => {
+      console.error(`${err} ❌`);
+      renderError(`Something went wrong`);
+    });
 };
-
-const WhereAmI = async function () {
-  try {
-    //Geolocation
-    const pos = await getPosition();
-    const { latitude: lat, longitude: lng } = pos.coords;
-
-    //Reverse Geo
-    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
-    if (!resGeo.ok) throw new Error("Problem getting location data");
-    const dataGeo = await resGeo.json();
-    console.log(dataGeo);
-
-    // Country data
-    const res = await fetch(
-      `https://restcountries.com/v3.1/name/${dataGeo.country}`
-    );
-    if (!res.ok) throw new Error("Problem getting location data");
-    const data = await res.json();
-    renderCountry(data[0]);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-WhereAmI();
-console.log("first");
+getCountryData("portugal");
